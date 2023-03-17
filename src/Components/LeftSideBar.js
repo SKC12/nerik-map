7ea4503ac2e9270236a2153ca20ab5d533167c56
SKC12 <stephancopetti@hotmail.com>
2023-03-17 17:00:03 -0300

    data sidebar

--- src/Components/LeftSideBar.js
+++ src/Components/LeftSideBar.js
@@ -6,6 +6,22 @@ function LeftSideBar(props) {
   const [selectedTab, setSelectedTab] = useState("dataTab");
   const selectedNode = props.selectedNode;
 
+  const selectedData = selectedNode
+    ? selectedNode.data
+    : {
+        sphere: "",
+        sphereRadius: "",
+        region: "",
+        flowRiver: "",
+        population: "",
+        activityLeve: "",
+        controlledBy: "",
+        description: "",
+        source: "",
+        creator: "",
+        website: "",
+      };
+
   const animationChange = () => {
     setIsAnimated(!isAnimated);
   };
@@ -61,8 +77,34 @@ function LeftSideBar(props) {
       </div>
       <div className="LEFTSB__inner-container">
         {selectedTab === "dataTab" ? (
-          <div>
-            <h2>Settings:</h2>
+          <div className="LEFTSB__data-container">
+            <label className="LEFTSB__data-label">Name:</label>
+            <p className="LEFTSB__data">{selectedData.sphere}</p>
+            <label className="LEFTSB__data-label">Sphere Radius:</label>
+            <p className="LEFTSB__data">{selectedData.sphereRadius}</p>
+            <label className="LEFTSB__data-label">Region:</label>
+            <p className="LEFTSB__data">{selectedData.region}</p>
+            <label className="LEFTSB__data-label">Flow River:</label>
+            <p className="LEFTSB__data">{selectedData.flowRiver}</p>
+            <label className="LEFTSB__data-label">Population:</label>
+            <p className="LEFTSB__data">{selectedData.population}</p>
+            <label className="LEFTSB__data-label">Activity Level:</label>
+            <p className="LEFTSB__data">{selectedData.activityLeve}</p>
+            <label className="LEFTSB__data-label">Controlled by:</label>
+            <p className="LEFTSB__data">{selectedData.controlledBy}</p>
+            <label className="LEFTSB__data-label">Description:</label>
+            <p className="LEFTSB__data">{selectedData.description}</p>
+            <label className="LEFTSB__data-label">Source:</label>
+            <p className="LEFTSB__data">{selectedData.source}</p>
+            <label className="LEFTSB__data-label">Creator:</label>
+            <p className="LEFTSB__data">{selectedData.creator}</p>
+            <label className="LEFTSB__data-label">Website:</label>
+            <a
+              href={selectedData.website}
+              className="LEFTSB__data LEFTSB__data-link"
+            >
+              {selectedData.website}
+            </a>
           </div>
         ) : null}
         {selectedTab === "editTab" ? (
